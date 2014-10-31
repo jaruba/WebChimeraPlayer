3e4e5ebb164ead451da90e3be03442c0c689a7aa
jaruba <phantom_on_line@yahoo.co.uk>
2014-11-01 00:48:15 +0200

    Update wJSNova.1.0.js

--- editor/wJSNova.1.0.js
+++ editor/wJSNova.1.0.js
@@ -12,7 +12,7 @@
  *
  ******************************************/
 
-function getUrlVars() {
+function getUrlVar() {
     var vars = {};
     var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
         vars[key] = value;
