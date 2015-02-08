6ef0a65dec282f218703feef27baff8f52506873
jaruba <phantom_on_line@yahoo.co.uk>
2015-02-08 16:45:30 +0200

    Changed to Pure JavaScript
    Used jquery function by mistake., changed to pure javascript.

--- player/webchimera.js
+++ player/webchimera.js
@@ -53,13 +53,6 @@ function IsJsonString(str) {
 }
 // end function to check if a string is json
 
-// check if plugin is installed
-var isInstalled = false;
-$.each(navigator.plugins, function() {
-	if (this.name == "WebChimera Plugin") isInstalled = true;
-});
-// end check if plugin is installed
-
 var wjs = function(context) {
     // Call the constructor
     return new wjs.init(context);
@@ -124,10 +117,12 @@ wjs.init.prototype.qmlLoaded = function(action) {
 
 wjs.init.prototype.addPlayer = function(qmlsettings) {
 	
-	if (!isInstalled) {
+	// check if plugin is installed
+	if (navigator.plugins["WebChimera Plugin"].name != "WebChimera Plugin") {
 		this.videoelem.innerHTML = '<iframe src="http://www.webchimera.org/no_plugin.php" scrolling="no" width="100%" height="100%" style="border: none"></iframe>';
 		return false;
 	}
+	// end check if plugin is installed
 
 	newid = (typeof qmlsettings["id"] === "undefined") ? "webchimera" : qmlsettings["id"]; // if no id set, default to "webchimera"
 
